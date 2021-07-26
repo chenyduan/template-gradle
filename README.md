f5f4437a7fabe1f8014df0ca0ddafc7a83fc4bee
snow-projects <chengcheng.duan@outlook.com>
2021-07-26 20:54:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gradle-parent
+ gradle7.x构建Java项目，使用gradle的版本管理插件
---
+++ gradle-user/src/main/java/org/test/MainApp.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class);
+    }
+
+}
